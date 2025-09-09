fe5c8f410088334725ca5bbd14224ea674f63368
vipinrao009 <vipinrao2018@gmail.com>
2025-09-09 14:51:31 +0530

    fix: correct button label from "Register" to "Login" in login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -72,7 +72,7 @@ export default function Login() {
                 <FaSpinner className="animate-spin" />
               </div>
             ) : (
-              "Register"
+              "Login"
             )}
           </button>
 
